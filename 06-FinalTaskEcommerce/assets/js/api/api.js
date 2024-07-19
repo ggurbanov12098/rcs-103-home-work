1b6b328491e285d90269c00ea2899b541974be82
Gabil Gurbanov <57041974+ggurbanov12098@users.noreply.github.com>
2024-07-20 00:32:47 +0400

    init miniProject

---
+++ 06-FinalTaskEcommerce/assets/js/api/api.js
@@ -0,0 +1,11 @@
+export const getData = async() => {
+    const res = await axios("https://fakestoreapi.com/products");
+    const data = await res.data;
+
+    return data;
+};
+
+// getData().then((data) => {
+//     console.log(data);
+// });
+    
---
+++ 06-FinalTaskEcommerce/assets/js/app.js
@@ -0,0 +1,9 @@
+const getData = async() => {
+    const res = await axios("https://fakestoreapi.com/products");
+    const data = await res.data;
+
+    return data;
+};
+
+getData().then((data) => {});
+
