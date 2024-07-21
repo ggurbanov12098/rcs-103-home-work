1edfaf9d560396c493a6a01a575068f5dd29a739
Gabil Gurbanov <57041974+ggurbanov12098@users.noreply.github.com>
2024-07-21 06:59:44 +0400

    Basket page
    func for count of each product added

--- 06-FinalTaskEcommerce/assets/js/basket.js
+++ 06-FinalTaskEcommerce/assets/js/basket.js
@@ -10,12 +10,40 @@ let resultBasketArr = [];
 
 getData().then(data => {
     // console.log(data.find((elem)=>elem.id===1));
-    JSON.parse(localStorage.getItem("basket")).forEach((basketID) => {
+    JSON.parse(localStorage.getItem("basket")).forEach((item) => {
         // console.log(data.find((elem) => elem.id === basketID));
-        resultBasketArr.push(data.find((elem) => elem.id === basketID))
+        console.log(item, "item");
+        resultBasketArr.push({...data.find((elem) => elem.id === item.id), count: item.count})
     });
 
     console.log(resultBasketArr, "resultBasketArr");
-    createCard(basketContainer, resultBasketArr);
+    // createCard(basketContainer, resultBasketArr);
+
+    console.log(basketArr);
+    // <h3>${elem.title.substring(0, 10)} - ${elem.price}</h3>
+    resultBasketArr.forEach((elem) => {
+        basketContainer.innerHTML += `
+            <div class="basketItem">
+                <h3>${elem.title} - ${elem.price}</h3>
+                <button>+</button>
+                <span>${elem.count}</span>
+                <button>-</button>
+            </div>;
+        `;
+    });
+
+
+
 });
 
+
+
+
+
+
+{/* <div class="basketItem">
+  <h3>name - price</h3>
+  <button>+</button>
+  <span>Count</span>
+  <button>-</button>
+</div>; */}
--- 06-FinalTaskEcommerce/assets/js/index.js
+++ 06-FinalTaskEcommerce/assets/js/index.js
@@ -8,11 +8,26 @@ getData().then((data) => {
     createCard(products, data);
     products.addEventListener("click", (e) => {
         if(e.target.classList.contains("addBasket")){
-            basket.push(+e.target.getAttribute("customID"));
-            console.log(basket, "basket");
-            localStorage.setItem("basket", JSON.stringify(basket));
-        }
+            // basket.push(+e.target.getAttribute("customID"));
+            
+            if(basket.find(elem => elem.id == e.target.getAttribute("customID"))){
+                let findElem = JSON.parse(localStorage.getItem("basket")).find(elem => elem.id == e.target.getAttribute("customID"))
+                findElem.count++
+                let newArr = basket.filter(elem=>elem.id!=findElem.id)
+                newArr.push(findElem);
+                localStorage.setItem("basket", JSON.stringify(newArr));
+            }else{
+                let obj = {
+                    id: +e.target.getAttribute("customID"),
+                    count: 1,
+                };
 
+                console.log(obj);
+                basket.push(obj);
+                console.log(basket, "basket");
+                localStorage.setItem("basket", JSON.stringify(basket));
+            }
+        }
     });
 });
 
